df20d8632b831c75f93b0e0bed94b9ae0736c16f
SriRajyaLakshmi38 <rajivure123@msitprogram.net>
2024-11-30 21:11:42 +0530

    com

---
+++ Avg.java
@@ -0,0 +1,13 @@
+class Avg{
+  public static void main(String[] args) {  
+  int a[] [] = new int[3][4];
+  System.out.println(avg(1,2));
+  }
+  static double avg(int n,int m){
+   double s = n+m;
+    return s/2;
+  
+  }
+
+}
+  
