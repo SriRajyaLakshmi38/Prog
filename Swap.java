a6f3e77b78096cb187fe6beef94dc6790c5e5fd7
SriRajyaLakshmi38 <rajivure123@msitprogram.net>
2024-12-01 16:15:41 +0530

    m

---
+++ Swap.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Swap {
+    
+
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        int a = Sc.nextInt();
+        int b = Sc.nextInt();
+       int temp = b;
+       b = temp;
+       
+       
+       System.out.println(b);
+       System.out.println(a);
+    }
+
+    
+
+    
+}
