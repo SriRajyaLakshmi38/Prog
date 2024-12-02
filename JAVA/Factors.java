06afccacb67c248f43cefef9540ae527cb780bb6
SriRajyaLakshmi38 <rajivure123@msitprogram.net>
2024-12-02 12:07:50 +0530

    30

---
+++ JAVA/Factors.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Factors {
+   public static void main(String[] args) {
+    Scanner SC = new Scanner(System.in);
+    int n = SC.nextInt();
+    int arr[] = {1};
+    for (int i = 2; i < (n/2); i++) {
+        if (n/i == 0) {
+        }
+    }
+
+
+}
+
+} 
