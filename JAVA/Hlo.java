c571a10bbfeefe3a12ce59c7865c96ff241af5d9
SriRajyaLakshmi38 <rajivure123@msitprogram.net>
2024-12-02 12:23:40 +0530

    35

--- JAVA/Hlo.java
+++ JAVA/Hlo.java
@@ -1,3 +1,16 @@
-public class Hlo {
-    
+class Hlo {
+   public static void main(String[] args) {
+    for (int i = 0; i <= 100; i++) {
+       if (i%5==0 && i%3==0) {
+        System.out.println("Hello Wolrd");
+       } else if(i%3==0) {
+        System.out.println("Hello");
+       } else if (i%5==0) {
+           System.out.println("Wolrd");   
+       }
+       else{
+        System.out.println("Sri");
+       }
+    }
+   }
 }
