e15c3b5a56f849e22818645e904faba0c8f81876
SriRajyaLakshmi38 <rajivure123@msitprogram.net>
2024-12-03 17:01:29 +0530

    x

--- JAVA/Factors.java
+++ JAVA/Factors.java
@@ -5,13 +5,32 @@ public class Factors {
    public static void main(String[] args) {
     Scanner SC = new Scanner(System.in);
     int n = SC.nextInt();
-    int arr[] = {1};
-    for (int i = 2; i < (n/2); i++) {
-        if (n/i == 0) {
+    int[] factors = new int[n];
+    int cnt = 0;
+    for (int i = 1; i <= (n/2); i++) {
+        if (n%i == 0){
+            factors[cnt] = i;
+            //System.out.println(factors);err
+            //System.out.printf("%d %d %d%n", 1, i, n);
+            cnt++;
         }
     }
-
-
+    for (int i = 0; i < cnt; i++) {
+        System.out.print(factors[i] + " ");
+    SC.close();
+   }
+}
 }
 
-} 
+
+
+
+
+
+
+
+
+
+
+
+
